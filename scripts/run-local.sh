fce315a053428f09d5ca6362c67d596bad6404db
Patacode <pata.codegineer@gmail.com>
2026-03-10 07:13:26 +0100

    feat(scripts): allow usage of personal github pat when running locally

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-bundle exec jekyll serve --livereload
+# constants
+readonly C_DEFAULT_GITHUB_PAT="$(cat $GITHUB_PAGES_PAT_LOC)"
+
+# args
+readonly A_GITHUB_PAT="${1:-$C_DEFAULT_GITHUB_PAT}"
+
+JEKYLL_GITHUB_TOKEN="$A_GITHUB_PAT" bundle exec jekyll serve --livereload
